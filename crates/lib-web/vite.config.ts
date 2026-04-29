96b77301416728e1abd1aa048008f3a2f9debccc
Nick Sikkema <contact@nicksikkema.com>
2026-04-28 21:29:47 -0500

    build: create basic docker image

--- crates/lib-web/vite.config.ts
+++ crates/lib-web/vite.config.ts
@@ -3,6 +3,8 @@ import {fileURLToPath, URL} from 'node:url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from "node:path";
+import license from 'rollup-plugin-license';
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
@@ -20,6 +22,29 @@ export default defineConfig(({ command }) => ({
         modulePreload: {
             polyfill: false,
         },
+        rollupOptions: {
+            plugins: [
+                license({
+                    thirdParty: {
+                        includePrivate: false,
+                        allow: {
+                            test: (dependency) => {
+                                // Return false for unlicensed dependencies.
+                                if (!dependency.license) {
+                                    return false
+                                }
+
+                                // Allow MIT and Apache-2.0 licenses.
+                                return ['MIT', 'Apache-2.0'].includes(dependency.license)
+                            },
+                            failOnUnlicensed: true,
+                            failOnViolation: true
+                        },
+                        output: path.resolve(__dirname, './dist/LICENSE.txt')
+                    }
+                })
+            ]
+        }
     },
     server: {
         host: '127.0.0.1',
